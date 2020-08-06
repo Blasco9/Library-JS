3cf2cfd92272b867dd90fa38b9a2835a4852ebec
Julian Blasco <blascoblasco9@gmail.com>
2020-08-06 19:25:11 -0300

    Modify addBookToLibrary function and createBook function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const col = document.querySelector('.col');
-const bookForm = document.querySelector('.book-form'); 
+const bookForm = document.querySelector('.book-form');
+let autoIncrementId = 0;
 let myLibrary = [
 	{
 		title: 'first',
@@ -31,17 +32,27 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
 	myLibrary.push(book);
+	let bookElement = createBook(book)
+	container.appendChild(bookElement)
+
+	bookElement.lastChild.addEventListener('click', function (e) {
+		let book = e.target.parentElement;
+
+		myLibrary.splice(book.id, 1);
+		container.removeChild(book);
+	});
 }
 
 function render() {
-	myLibrary.forEach((book, i) => {
-		container.innerHTML += `<div class="book" id="${i}">
+	myLibrary.forEach((book) => {
+		container.innerHTML += `<div class="book" id="${autoIncrementId}">
       <p>${book.title}</p>
       <p>${book.author}</p>
       <p>${book.pages}</p>
 			<p>${book.read}</p>
 			<span class="remove-btn">REMOVE</span>
-    </div>`;
+		</div>`;
+		autoIncrementId++;
 	});
 }
 
@@ -51,25 +62,57 @@ const books = document.querySelectorAll('.book');
 
 function addEventListeners() {
 	Array.from(books).forEach((book) => {
-		book.addEventListener('click', function(e) {
-		});
+		book.addEventListener('click', function (e) {});
 	});
 }
 
-addEventListeners()
+addEventListeners();
 
-col.addEventListener("click",function(){
-	bookForm.style.display = "block";
-})
+col.addEventListener('click', function () {
+	bookForm.classList.remove('d-none');
+});
 
-const removeBtns = document.querySelectorAll('.remove-btn')
+const removeBtns = document.querySelectorAll('.remove-btn');
 
 Array.from(removeBtns).forEach((btn) => {
-	btn.addEventListener('click', function(e) {
-		let book = e.target.parentElement
-	
-		console.log(book);
-		myLibrary.splice(book.id, 1)
-		container.removeChild(book)
-	})
-})
+	btn.addEventListener('click', function (e) {
+		let book = e.target.parentElement;
+
+		myLibrary.splice(book.id, 1);
+		container.removeChild(book);
+	});
+});
+
+document.querySelector('.create-book-btn').addEventListener('click', function () {
+	let book = {
+		title: bookForm.children[0].value,
+		author: bookForm.children[1].value,
+		pages: bookForm.children[2].value,
+		read: bookForm.children[3].value,
+	};
+	addBookToLibrary(book);
+});
+
+function createBook(book) {
+	let bookDiv = document.createElement('div')
+	let title = document.createElement('p')
+	title.innerText = book.title
+	let author = document.createElement('p')
+	author.innerText = book.author
+	let pages = document.createElement('p')
+	pages.innerText = book.pages
+	let read = document.createElement('p')
+	read.innerText = book.read
+	let removeBtn = document.createElement('span')
+	removeBtn.innerText = "REMOVE"
+	removeBtn.className = 'remove-btn'
+	bookDiv.appendChild(title)
+	bookDiv.appendChild(author)
+	bookDiv.appendChild(pages)
+	bookDiv.appendChild(read)
+	bookDiv.appendChild(removeBtn)
+	bookDiv.classList.add('book')
+	bookDiv.id = autoIncrementId++
+
+	return bookDiv
+}
