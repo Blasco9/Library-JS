adca4278cc23dd6d7d85c84276889adb36ab1666
Julian Blasco <blascoblasco9@gmail.com>
2020-08-07 13:24:41 -0300

    Refactor addBookToLibrary, removeBook functions and delete createBook function

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
 const container = document.querySelector('.container');
-const col = document.querySelector('.col');
+const newBookBtn = document.querySelector('.new-book-btn');
 const bookForm = document.querySelector('.book-form');
-let autoIncrementId = 0;
-let myLibrary = [
-	{
-		title: 'first',
-		author: '2',
-		pages: '3',
-		read: '4',
-	},
-	{
-		title: 'second',
-		author: '2',
-		pages: '3',
-		read: '4',
-	},
-	{
-		title: 'third',
-		author: '2',
-		pages: '3',
-		read: '4',
-	},
-];
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -32,55 +12,36 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
 	myLibrary.push(book);
-	let bookElement = createBook(book)
-	container.appendChild(bookElement)
 
-	bookElement.lastChild.addEventListener('click', function (e) {
-		let book = e.target.parentElement;
-
-		myLibrary.splice(book.id, 1);
-		container.removeChild(book);
-	});
+	container.innerHTML += `<div id="${myLibrary.length}" class="book">
+														<p>${book.title}</p>
+														<p>${book.author}</p>
+														<p>${book.pages}</p>
+														<p>${book.read}</p>
+														<span class="remove-btn" onclick="removeBook(this)">REMOVE</span>
+													</div>`;
 }
 
 function render() {
 	myLibrary.forEach((book) => {
-		container.innerHTML += `<div class="book" id="${autoIncrementId}">
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <p>${book.pages}</p>
-			<p>${book.read}</p>
-			<span class="remove-btn">REMOVE</span>
-		</div>`;
-		autoIncrementId++;
+		container.innerHTML += `<div id="${myLibrary.length}" class="book"">
+    												  <p>${book.title}</p>
+    												  <p>${book.author}</p>
+    												  <p>${book.pages}</p>
+															<p>${book.read}</p>
+															<span class="remove-btn" onclick="removeBook(this)">REMOVE</span>
+														</div>`;
 	});
 }
 
-render();
-
-const books = document.querySelectorAll('.book');
-
-function addEventListeners() {
-	Array.from(books).forEach((book) => {
-		book.addEventListener('click', function (e) {});
-	});
+function removeBook(btn) {
+	let bookToRemove = btn.parentElement;
+	myLibrary.splice(bookToRemove.id, 1);
+	container.removeChild(bookToRemove);
 }
 
-addEventListeners();
-
-col.addEventListener('click', function () {
+newBookBtn.addEventListener('click', function () {
 	bookForm.classList.remove('d-none');
-});
-
-const removeBtns = document.querySelectorAll('.remove-btn');
-
-Array.from(removeBtns).forEach((btn) => {
-	btn.addEventListener('click', function (e) {
-		let book = e.target.parentElement;
-
-		myLibrary.splice(book.id, 1);
-		container.removeChild(book);
-	});
 });
 
 document.querySelector('.create-book-btn').addEventListener('click', function () {
@@ -93,26 +54,4 @@ document.querySelector('.create-book-btn').addEventListener('click', function ()
 	addBookToLibrary(book);
 });
 
-function createBook(book) {
-	let bookDiv = document.createElement('div')
-	let title = document.createElement('p')
-	title.innerText = book.title
-	let author = document.createElement('p')
-	author.innerText = book.author
-	let pages = document.createElement('p')
-	pages.innerText = book.pages
-	let read = document.createElement('p')
-	read.innerText = book.read
-	let removeBtn = document.createElement('span')
-	removeBtn.innerText = "REMOVE"
-	removeBtn.className = 'remove-btn'
-	bookDiv.appendChild(title)
-	bookDiv.appendChild(author)
-	bookDiv.appendChild(pages)
-	bookDiv.appendChild(read)
-	bookDiv.appendChild(removeBtn)
-	bookDiv.classList.add('book')
-	bookDiv.id = autoIncrementId++
-
-	return bookDiv
-}
+render();
